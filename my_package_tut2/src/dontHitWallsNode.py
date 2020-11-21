68bd5266eff14f81aac6bad8bfc24c320c52c7d0
Katja Grünewald <ge93tef@mytum.de>
2020-11-21 20:20:12 +0100

    Stoppes before wall, finally

--- my_package_tut2/src/dontHitWallsNode.py
+++ my_package_tut2/src/dontHitWallsNode.py
@@ -7,21 +7,6 @@ from pcimr_simulation.srv import InitPos
 from geometry_msgs.msg import Twist
 import numpy as np
 
-global pub_velo
-global maxVelo
-global minDist
-global global_velo
-global global_velo_linear
-global maxDist
-pub_velo = Twist()
-pub_velo.linear.x=0.0
-pub_velo.linear.y=0.0
-pub_velo.linear.z=0.0
-pub_velo.angular.x=0.0
-pub_velo.angular.y=0.0
-pub_velo.angular.z=0.0
-global_velo = pub_velo
-global_velo_linear = np.array([pub_velo.linear.x,pub_velo.linear.y ,pub_velo.linear.z]) #Initial velo as array
 
 #Scan around me
 def callback_Scan(data):
@@ -29,8 +14,15 @@ def callback_Scan(data):
     global_scan = np.array(data.ranges)
 
 def callback_Velo(data):
+    global global_velo
+    global global_velo_linear
     global_velo= [data.linear.x, data.linear.y, data.linear.z]  
     global_velo_linear = np.array([data.linear.x,data.linear.y ,data.linear.z]) 
+    if((global_velo_linear > maxVelo).any()):
+        print("Max speed exceeded", global_velo_linear)
+        setMaxVelo(global_velo_linear)
+        velo_move.publish(pub_velo)
+    
 
 #Any distance is beneath the min Distance = Stop everything
 def emergencyStop():
@@ -41,67 +33,78 @@ def emergencyStop():
     pub_velo.angular.y=0.0
     pub_velo.angular.z=0.0
 
-def setMaxVelo():
-    if(pub_velo.linear.x > maxVelo ):
+def setMaxVelo(global_velo_linear):
+    if(global_velo_linear[0] > maxVelo ):
         pub_velo.linear.x = maxVelo
-    if(pub_velo.linear.y > maxVelo):
+    if(global_velo_linear[1]  > maxVelo):
         pub_velo.linear.y = maxVelo
-    if(pub_velo.linear.z > maxVelo):
+    if(global_velo_linear[2]  > maxVelo):
         pub_velo.linear.z= maxVelo
 
 def adjustSpeed():
+    actualDist = np.amin(global_scan)-minDist
     rangeDist = maxDist-minDist # Allowed  Range
-    newVelo = maxVelo*(rangeDist/maxDist)#Calc new Velo
-    #If any Velo is above new velo --> adjust
-    if(pub_velo.linear.x > newVelo ):
+    #print("actualDist", actualDist)
+    newVelo = maxVelo*(actualDist/rangeDist)#Calc new Velo
+    #If any Velo is above newVelo --> adjust
+    if(global_velo_linear[0]> newVelo ):
         pub_velo.linear.x = newVelo
-    if(pub_velo.linear.y > newVelo):
+    if(global_velo_linear[1]> newVelo):
         pub_velo.linear.y = newVelo
-    if(pub_velo.linear.z > newVelo):
+    if(global_velo_linear[2] > newVelo):
         pub_velo.linear.z= newVelo
 
 
 def checkWall():
+    #GlobalValues
     global pub_velo
     global maxVelo
     global minDist
     global global_velo
-    global global_velo_linear
     global maxDist
     global global_scan
-    maxVelo = 1.0 #max velocity
-    minDist = 0.10 #min DIstance to object
-    maxDist = 3.0 #Distance until the robot can drive with maxVelo
+    global global_velo_linear
+    global velo_move
+    global_velo_linear=np.array([0.0,0.0,0.0])
+    #InitValues
+    pub_velo = Twist()
+    pub_velo.linear.x=0.0
+    pub_velo.linear.y=0.0
+    pub_velo.linear.z=0.0
+    pub_velo.angular.x=0.0
+    pub_velo.angular.y=0.0
+    pub_velo.angular.z=0.0
+    maxVelo = 0.2 #max velocity
+    minDist = 0.30 #min DIstance to object
+    maxDist = 2.0 #Distance until the robot can drive with maxVelo
+
+    #Subscribers and Publisher
     scan = rospy.Subscriber('/scan', LaserScan, callback_Scan)
-    velo_move = rospy.Publisher('cmd_vel', Twist, queue_size=10)
-    velo = rospy.Subscriber('cmd_vel', Twist, callback_Velo)
+    #velo_move = rospy.Publisher('input/cmd_vel', Twist, queue_size=10)
+    velo = rospy.Subscriber('/pioneer/cmd_vel', Twist, callback_Velo)
+    velo_move = rospy.Publisher('/pioneer/cmd_vel', Twist, queue_size=10)
+    
 
     rospy.init_node('checkWall', anonymous=True)
-    rate = rospy.Rate(1)
+    rate = rospy.Rate(2)
 
 
     subrate = rospy.Rate(1)
-    pubrate = rospy.Rate(1)
+    pubrate = rospy.Rate(0.5)
 
-    while not rospy.is_shutdown():
+    while (not rospy.is_shutdown()):
         subrate.sleep()
         #Check if robot is already to close
+        print('Velocity', global_velo_linear)
         if((global_scan < minDist).any()):
             emergencyStop()
+            print("Emergency Stop")
             velo_move.publish(pub_velo)
-            print("Robot Stopped")
-        #Check if any linear velocity reached max
-        elif((global_velo_linear > maxVelo).any()):
-            setMaxVelo()
-            velo_move.publish(pub_velo)
+            #rospy.signal_shutdown("Emergency Stop")
         #Adjust speed relative to distance
         elif((global_scan < maxDist).any()):
             adjustSpeed()
             velo_move.publish(pub_velo)
-        #velo_move.publish(pub_velo)
-        #print("robot scan", global_scan)
-        print("global_velo", global_velo)
-        # rospy.signal_shutdown("Done")
 
 if __name__ == '__main__':
     checkWall()
