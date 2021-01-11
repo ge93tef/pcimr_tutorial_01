bde374886bf7e128d6b71a8d585781f061d1bd0f
ge93tef <ge93tef@mytum.de>
2021-01-11 14:24:50 +0100

    Far better

--- final_project/src/reinforcmentLearning.py
+++ final_project/src/reinforcmentLearning.py
@@ -28,13 +28,13 @@ import numpy as np
 class learnToMove:
 
     def __init__(self):
-        self.learnRate = rospy.get_param('~/learnRate', 0.85) #LearnRate
-        self.epsilon = rospy.get_param('~/epsilon', 0.2) #Randomness
-        self.gamma = rospy.get_param('~/gamma', 0.9) #forQCalc
+        self.learnRate = rospy.get_param('~/learnRate', 0.7) #LearnRate
+        self.epsilon = rospy.get_param('~/epsilon', 0.4) #Randomness
+        self.gamma = rospy.get_param('~/gamma', 0.85) #forQCalc
         self.startVelocity = rospy.get_param('~/startVelocity', [0.1,0.0,0.0])
-        self.actionValues = rospy.get_param('~/actionValues', [0.1,0.25,0.25,0.25])
+        self.actionValues = rospy.get_param('~/actionValues', [0.3,0.1,0.4,0.5])
         self.maxEpisodes = rospy.get_param('~/maxEpisodes', 5)
-        self.amountBlocks = rospy.get_param('~/amountBlocks', 5)
+        self.amountBlocks = rospy.get_param('~/amountBlocks', 8)
         self.newVelo = Twist()
         self.velo_move = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
         self.move_sub = rospy.Subscriber('/input/cmd_vel', Twist, self.callbackMove)
@@ -79,56 +79,62 @@ class learnToMove:
     def get_distance(self):
         self.msg = rospy.wait_for_message("scan", LaserScan)
         self.scan = np.round(np.array(self.msg.ranges), 1)
-        self.actions = np.array(['Forward','SpeedX','MoveLeft', 'MoveRight']) #See above
+        self.actions = np.array(['LeftTurn', 'Speed0', 'RightTurn', 'Speed1']) #See above
         self.stateSpace = self.getSpaceState(self.scan)
         self.QTable = np.zeros((2, self.actions.shape[0] ))
-        self.rewards = np.array([-30,0.001,-5,-10,5, 0.02]) # See above
+        self.rewards = np.array([0.04,0.02,0.01,-0.1, 0.1]) # outerDistance, InnerDistance, Straight, TooCloseToWall, Speed
         self.reset_simulation = rospy.ServiceProxy('/gazebo/reset_simulation', Empty)
         self.reset_simulation()
 
     def checkDistance(self):
         self.msg = rospy.wait_for_message("scan", LaserScan)
         self.scan = np.round(np.array(self.msg.ranges), 1)
-        return np.amin(self.scan[61:184])
+        #return np.amin(self.scan[61:184])
 
     
     def makeAction(self, action, actionValues):
         reward = 0
+        lost = False
         #print(action)
-        if(action == 'Forward'):
+        if(action =='Speed1'):
             self.newVelo.angular.z = 0.0
-        elif(action == 'SpeedX'):    
-            self.newVelo.linear.x = self.newVelo.linear.x + actionValues[0]
-            if(self.newVelo.linear.x  > 0.5):
-                self.newVelo.linear.x = 0.5
-            else:
-                reward = reward + self.rewards[1] #Reward for Speed
-        elif(action == 'SlowX'):
-            self.newVelo.linear.x = self.newVelo.linear.x - actionValues[1]
-            if(self.newVelo.linear.x <= 0):
-                self.newVelo.linear.x = 0
-                reward = reward + self.rewards[3]
-        elif(action == 'MoveLeft'):
-            self.newVelo.angular.z= -0.40
-            self.newVelo.linear.x = 0
-            #reward = reward + self.rewards[1] #Reward for Speed
-        elif(action == 'MoveRight'):
-            #reward = reward + self.rewards[2] #Reward for Slow
-            self.newVelo.angular.z = 0.40
-            self.newVelo.linear.x = 0
-        if(self.newVelo.linear.x > 0):
-            reward = reward + self.rewards[1] #Reward for Speed
+            self.newVelo.linear.x = actionValues[2]
+        if(action == 'Speed0'):
+            self.newVelo.angular.z = 0.0
+            self.newVelo.linear.x = actionValues[0]
+        elif(action == 'LeftTurn'):
+            self.newVelo.angular.z= 0.10
+            self.newVelo.linear.x = actionValues[1]
+        elif(action == 'RightTurn'):
+            self.newVelo.angular.z = -0.10
+            self.newVelo.linear.x = actionValues[1]
         self.velo_move.publish(self.newVelo)  #Publish
-        minDist = self.checkDistance()
-        if(minDist <= 0.45):
-            reward = reward + self.rewards[0] #RewardForHitWall
-        if(minDist > 0.45):
-            reward = reward + self.rewards[4]
-        if(minDist <= 0.14):
-            reward = reward + 80*(self.rewards[0])
+        self.checkDistance() #checkDistanceForRewards
+        #CheckSpeed
+        if(self.newVelo.linear.x > 0.1):
+            reward = reward + self.newVelo.linear.x *self.rewards[4]
+        #Check Outer
+        if(self.scan[33]== self.scan[212]):
+            reward = reward + self.rewards[0]
+        #Punish Difference
+        else:
+            diff = np.abs(self.scan[33]-self.scan[212])
+            reward = reward -diff *self.rewards[4]
+        #Check Straight
         if(self.scan[120]>0.7):
-            reward = reward + 0.1
-        return reward, False
+            reward = reward + self.rewards[2]
+        #HitWall
+        if(np.amin(self.scan)<=0.14):
+            print('Lost Game Collision')
+            lost=True
+        if(self.scan[33]>2.0 or self.scan[212]>2.0):
+            lost=True
+            print('Lost Game Rotation')
+        if(np.amin(self.scan)<=0.3):
+            reward = reward + self.rewards[3]
+        if(np.amin(self.scan[33:212])<=0.3):
+            reward = reward + self.rewards[3]
+        return reward, lost
 
     def learn(self):
         steps = 0
@@ -136,34 +142,50 @@ class learnToMove:
         overallReward = 0
         currentState = self.stateSpace[1]
         done = False
+        lost = False
         print("Episode", episode)
         while not done:
             done = False
-            if(overallReward < -10000):
+            if(lost):
                 episode = episode +1
                 print("Episode", episode)
                 print('Steps', steps)
-                self.reset_simulation()
-                overallReward = 0
+                print('overallReward', overallReward+100)
+                self.reset_simulation() 
                 currentState = self.stateSpace[1]
                 self.newVelo.linear.x = 0
                 self.newVelo.angular.z = 0
                 steps = 0
+                lost=False
+                if(episode > 4):
+                    self.epsilon = 0.3
+                if(overallReward > 10):
+                    self.epsilon = 0.1
+                if(overallReward > 50):
+                    self.epsilon = 0.001
+                overallReward = 0
             if random.uniform(0,1)<self.epsilon:
                 actionId = random.randrange(self.actions.shape[0]) 
                 chooseAction = self.actions[actionId]       
             else:
                 currentState,stateId, self.stateSpace, self.QTable=self.getState(currentState, self.stateSpace,self.QTable)
-                actionId = np.argmax(self.QTable[stateId, :])
+                line = self.QTable[stateId, :]
+                if(np.count_nonzero(line)==line.shape[0]):
+                    actionId = random.randrange(self.actions.shape[0]) 
+                else:
+                    actionId = np.argmax(self.QTable[stateId, :])
                 chooseAction = self.actions[actionId]  
             oldState=np.copy(currentState)
-            myReward, done = self.makeAction(chooseAction,self.actionValues)
-            #myReward = myReward + steps * self.rewards[5] ##Longer run = more points
+            myReward, lost = self.makeAction(chooseAction,self.actionValues)
+            if(lost):
+                myReward = -300
             currentState,stateId, self.stateSpace, self.QTable=self.getState(self.scan , self.stateSpace, self.QTable)
             oldState,oldStateId,self.stateSpace, self.QTable=self.getState(oldState, self.stateSpace, self.QTable)
             self.QTable[oldStateId, actionId] = self.QTable[oldStateId, actionId] + self.learnRate * (myReward+self.gamma*np.max(self.QTable[stateId, :]) - self.QTable[oldStateId,actionId])
             steps = steps+1
             overallReward = overallReward + myReward
+            if(steps >  15000):
+                lost=True
 
 
 
